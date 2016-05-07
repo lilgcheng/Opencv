53f92f6340d53e683eedcc2dee958a0fd1a5f1d3
dbCheng <lilgcheng@gmail.com>
2016-05-07 09:14:27 +0800

    Added files via upload

--- Opencv_Tutorial/find_beans/show_image.cpp
+++ Opencv_Tutorial/find_beans/show_image.cpp
@@ -3,71 +3,42 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <cmath> 
-#include <iomanip> 
-#include <iostream> 
-#include <stdlib.h> 
 using namespace cv;
 using namespace std;
 void colorReduce(Mat&image, int n);
 int main(int argc, char **argv)
 {
-	Mat image = imread("test.jpg", CV_LOAD_IMAGE_COLOR);
-	double c = 0;
+	Mat image = imread("bean.jpg", CV_LOAD_IMAGE_COLOR);
+
 
 	int iLowH = 0;
-	int iHighH = 70;
+	int iHighH = 40;
 
-	int iLowS = 0; 
+	int iLowS = 40; 
 	int iHighS = 255;
 
 	int iLowV = 0;
-	int iHighV = 70;
+	int iHighV = 255;
 
 	if(!image.data){
 		cout<<"no find image"<<endl;
 	}
-	namedWindow("image",CV_WINDOW_AUTOSIZE);//AUTOSIZE
-	imshow("image",image);
+
 
 
 	Mat imgHSV;
+
 	cvtColor(image, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
 	Mat imgThresholded;
+
 	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+	namedWindow("image",CV_WINDOW_AUTOSIZE);//AUTOSIZE
+	imshow("image",image);
+	
 	namedWindow("imgHSV",CV_WINDOW_AUTOSIZE);//AUTOSIZE
 	imshow("imgHSV",imgThresholded);
-	// cout<<imgThresholded.rows<<endl;
-	// cout<<imgThresholded.cols<<endl;
-	int i,j;  
-	int cPointR,cPointG,cPointB,cPoint;//currentPoint;  
-	for(i=1;i<imgThresholded.rows;i++){  
-		for(j=1;j<imgThresholded.cols;j++){  
-			cPointB=imgThresholded.at<Vec3b>(i,j)[0];  
-			cPointG=imgThresholded.at<Vec3b>(i,j)[1];  
-			cPointR=imgThresholded.at<Vec3b>(i,j)[2];  
-			cout<<"B = "<<cPointB<<endl;
-			cout<<"G = "<<cPointG<<endl;
-			cout<<"R = "<<cPointR<<endl;
-			//255 = white and 0 = black
-			if(cPointB == 255 &cPointR ==255  &cPointG == 255 )  {  
-				c++;
-				imgThresholded.at<Vec3b>(i,j)[0]=0;  
-				imgThresholded.at<Vec3b>(i,j)[1]=0;  
-				imgThresholded.at<Vec3b>(i,j)[2]=0;  
-			}
-		}  
-	}  
-	double percentage ; 
-	double total = image.rows*image.cols;
-	percentage = c/total;
-	cout<<"total = "<<total<<endl;
-	cout<<"black piex = "<<c<<endl;
-	cout <<  percentage << endl; 
-	namedWindow("image1",CV_WINDOW_AUTOSIZE);//AUTOSIZE
-	imshow("image1",imgThresholded);
-	
-
 
 	//colorReduce(image,64);
 	
